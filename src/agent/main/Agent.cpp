tizenorg/platform.core.security.askuser

Add main agent class
Agent class has been added and its usage in main.cpp file.

Change-Id: Iaec77d164c966a389df97e415febdca6622dc082

src/agent/main/Agent.cpp
@@ -1,0 +1,59 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/agent/main/Agent.cpp
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @brief       This file implements main class of ask user agent
+ */
+
+#include <unistd.h>
+
+#include <log/log.h>
+
+#include "Agent.h"
+
+namespace AskUserAgent {
+
+Agent::Agent() {
+    init();
+}
+
+Agent::~Agent() {
+    finish();
+}
+
+void Agent::init() {
+    // TODO: implement if needed
+
+    LOGD("Agent daemon initialized");
+}
+
+void Agent::run() {
+    // TODO: implement real task
+    while (true) {
+        sleep(1);
+    }
+
+    LOGD("Ask user agent task stopped");
+}
+
+void Agent::finish() {
+    // TODO: implement if needed
+
+    LOGD("Agent daemon has stopped commonly");
+}
+
+} // namespace AskUserAgent

src/agent/main/Agent.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/agent/main/Agent.h
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @brief       This file defines main class of ask user agent
+ */
+
+#pragma once
+
+namespace AskUserAgent {
+
+class Agent {
+public:
+    Agent();
+    ~Agent();
+
+    void run();
+
+private:
+    void init();
+    void finish();
+};
+
+} // namespace AskUserAgent
+

Samsung/askuser

src/agent/main/Agent.cpp
@@ -1,0 +1,63 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/agent/main/Agent.cpp
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @brief       This file implements main class of ask user agent
+ */
+
+#include <unistd.h>
+
+#include <log/log.h>
+
+#include "Agent.h"
+
+namespace AskUser {
+
+namespace Agent {
+
+Agent::Agent() {
+    init();
+}
+
+Agent::~Agent() {
+    finish();
+}
+
+void Agent::init() {
+    // TODO: implement if needed
+
+    LOGD("Agent daemon initialized");
+}
+
+void Agent::run() {
+    // TODO: implement real task
+    while (true) {
+        sleep(1);
+    }
+
+    LOGD("Ask user agent task stopped");
+}
+
+void Agent::finish() {
+    // TODO: implement if needed
+
+    LOGD("Agent daemon has stopped commonly");
+}
+
+} // namespace Agent
+
+} // namespace AskUser

src/agent/main/Agent.h
@@ -1,0 +1,42 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * @file        src/agent/main/Agent.h
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @brief       This file defines main class of ask user agent
+ */
+
+#pragma once
+
+namespace AskUser {
+
+namespace Agent {
+
+class Agent {
+public:
+    Agent();
+    ~Agent();
+
+    void run();
+
+private:
+    void init();
+    void finish();
+};
+
+} // namespace Agent
+
+} // namespace AskUser