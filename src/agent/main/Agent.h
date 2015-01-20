tizenorg/platform.core.security.askuser

Add UI handling
This patch introduces user interface related stuff, implements
creating UIs and handling responses from them.

Change-Id: I8899ead4d4b6222bf6c50bb51a2703502e2f7210

src/agent/main/Agent.h
@@ -24,11 +24,15 @@
 #include <condition_variable>
 #include <map>
 #include <mutex>
+#include <types/PolicyType.h>
 
 #include <containers/ConcurrentQueue.h>
 
 #include <main/CynaraTalker.h>
 #include <main/Request.h>
+#include <main/Response.h>
+
+#include <ui/AskUIInterface.h>
 
 namespace AskUser {
 
@@ -45,15 +49,22 @@ private:
     CynaraTalker m_cynaraTalker;
     std::map<RequestId, Request> m_requests;
     AskUser::ConcurrentQueue<Request> m_incomingRequests;
+    AskUser::ConcurrentQueue<Response> m_incomingResponses;
     std::condition_variable m_event;
     std::mutex m_mutex;
+    std::map<RequestId, AskUIInterfacePtr> m_UIs;
 
     void init();
     void finish();
 
     void requestHandler(const Request &request);
+    void UIResponseHandler(RequestId requestId, UIResponseType responseType);
+
     void processCynaraRequest(const Request &request);
     bool startUIForRequest(const Request &request);
+    bool cleanupUIThreads();
+
+    static Cynara::PolicyType UIResponseToPolicyType(UIResponseType responseType);
 };
 
 } // namespace Agent

src/agent/ui/AskUIInterface.h
@@ -1,0 +1,58 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        src/ui/AskUIInterface.h
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @brief       This file contains ask user UI interface declaration.
+ */
+
+#pragma once
+
+#include <functional>
+#include <memory>
+
+#include <main/Request.h>
+
+namespace AskUser {
+
+namespace Agent {
+
+typedef enum {
+    URT_YES,
+    URT_NO,
+    URT_SESSION,
+    URT_TIMEOUT,
+    URT_ERROR
+} UIResponseType;
+
+typedef std::function<void(RequestId, UIResponseType)> UIResponseCallback;
+
+class AskUIInterface {
+public:
+    virtual ~AskUIInterface() {};
+
+    virtual bool start(const std::string &client, const std::string &user,
+                       const std::string &privilege, RequestId requestId, UIResponseCallback) = 0;
+    virtual bool setOutdated() = 0;
+    virtual bool dismiss() = 0;
+    virtual bool isDismissing() = 0;
+};
+
+typedef std::shared_ptr<AskUIInterface> AskUIInterfacePtr;
+
+} // namespace Agent
+
+} // namespace AskUser

Samsung/askuser

src/agent/main/Agent.h
@@ -26,9 +26,13 @@
 #include <map>
 #include <mutex>
 #include <queue>
+#include <types/PolicyType.h>
 
 #include <main/CynaraTalker.h>
 #include <main/Request.h>
+#include <main/Response.h>
+
+#include <ui/AskUIInterface.h>
 
 namespace AskUser {
 
@@ -49,9 +53,11 @@ private:
     CynaraTalker m_cynaraTalker;
     std::map<RequestId, Request *> m_requests;
     std::queue<Request *> m_incomingRequests;
+    std::queue<Response> m_incomingResponses;
     std::condition_variable m_event;
     std::mutex m_mutex;
     static volatile sig_atomic_t m_stopFlag;
+    std::map<RequestId, AskUIInterfacePtr> m_UIs;
 
     void init();
     void finish();
@@ -59,6 +65,13 @@ private:
     void requestHandler(Request *request);
     void processCynaraRequest(Request *request);
     bool startUIForRequest(Request *request);
+    void UIResponseHandler(RequestId requestId, UIResponseType responseType);
+
+    void processUIResponse(const Response &response);
+    bool cleanupUIThreads();
+    void dismissUI(RequestId requestId);
+
+    static Cynara::PolicyType UIResponseToPolicyType(UIResponseType responseType);
 };
 
 } // namespace Agent

src/agent/ui/AskUIInterface.h
@@ -1,0 +1,59 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        AskUIInterface.h
+ * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @brief       This file contains ask user UI interface declaration.
+ */
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <main/Request.h>
+
+namespace AskUser {
+
+namespace Agent {
+
+typedef enum {
+    URT_YES,
+    URT_NO,
+    URT_SESSION,
+    URT_TIMEOUT,
+    URT_ERROR
+} UIResponseType;
+
+typedef std::function<void(RequestId, UIResponseType)> UIResponseCallback;
+
+class AskUIInterface {
+public:
+    virtual ~AskUIInterface() {};
+
+    virtual bool start(const std::string &client, const std::string &user,
+                       const std::string &privilege, RequestId requestId, UIResponseCallback) = 0;
+    virtual bool setOutdated() = 0;
+    virtual bool dismiss() = 0;
+    virtual bool isDismissing() = 0;
+};
+
+typedef std::unique_ptr<AskUIInterface> AskUIInterfacePtr;
+
+} // namespace Agent
+
+} // namespace AskUser